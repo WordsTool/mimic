264712d7eb865425c4b18a799a48bad4b559cbe5
Viktor Kostrov <kostrovvn@gmail.com>
2021-02-23 14:09:33 +0300

    removes unsupported and adds the appeared of dictionaries config

--- src/app/core/SettingsStorage.ts
+++ src/app/core/SettingsStorage.ts
@@ -1,5 +1,6 @@
 import StorageSettingsType = mimic.StorageSettingsType;
 import UpdateCommonSettings = mimic.UpdateCommonSettings;
+import DictionaryConfig = mimic.DictionaryConfig;
 
 const STORAGE_NAME = 'MIMIC_DICTIONARY_SETTINGS';
 
@@ -42,10 +43,31 @@ export default class SettingsStorage {
     });
   }
 
+  // removes unsupported and adds the appeared of dictionaries config
+  private updateDictionariesConfig() {
+    const initialList: DictionaryConfig[] = [...this.initial.dictionariesConfig];
+    const currentList: DictionaryConfig[] = [];
+
+    this.current.dictionariesConfig.forEach((item) => {
+      const { id: currentId } = item;
+      const index = initialList.findIndex(({ id }) => id === currentId);
+
+      if (index !== -1) {
+        currentList.push(item); // add if in initial list
+        initialList.splice(index, 1); // remove from initial list
+      }
+    });
+
+    // concat current config list and rest initial dictionaries
+    this.current.dictionariesConfig = [...currentList, ...initialList];
+  }
+
   public async fetch(): Promise<StorageSettingsType> {
     return new Promise((resolve) => {
       chrome.storage.sync.get([STORAGE_NAME], (result) => {
         this.current = result && result[STORAGE_NAME] ? result[STORAGE_NAME].data : this.initial;
+
+        this.updateDictionariesConfig();
 
         resolve(this.current);
       });
