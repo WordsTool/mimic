5fbbdbdd0f5c3bc803428f660ce8a5ed597c30b3
Viktor Kostrov <kostrovvn@gmail.com>
2021-02-25 02:10:50 +0300

    highlight question

--- src/app/components/popup/help/faq/FaqItem.tsx
+++ src/app/components/popup/help/faq/FaqItem.tsx
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 import Typography from '../../../base/Typography';
 
 
-export const FaqItemQuestion = styled(Typography).attrs({ variant: 'body1', color: 'dark' })`
+export const FaqItemQuestion = styled(Typography).attrs({ variant: 'body1' })`
   margin: 0 0 8px 0;
+  font-weight: bold;
 `;
 
-export const FaqItemAnswer = styled(Typography).attrs({ variant: 'body2' })`
+export const FaqItemAnswer = styled(Typography).attrs({ variant: 'body2', color: 'dark' })`
 `;
 
 const FaqItem = styled(Typography).attrs({ variant: 'body2' })`
